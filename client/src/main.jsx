acc60a5b22e61eb4d3f2e82aa40d675944eb65c0
Karan Solanki <20240526+solan117@users.noreply.github.com>
2025-06-21 16:43:27 -0400

    Add AppContext with Provider and wrap App with context and BrowserRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
+import {AppContextProvider} from "./context/AppContext.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-        <Router>
-            <App />
-        </Router>
-
+        <BrowserRouter>
+            <AppContextProvider>
+                <App />
+            </AppContextProvider>
+        </BrowserRouter>
   </StrictMode>,
 )
