66ea75bb930239cca09e9c2dfcd97323e56b4ff1
Karan Solanki <20240526+solan117@users.noreply.github.com>
2025-06-21 18:34:28 -0400

    Add Hero component with search functionality and update Home page structure, revise HTML layout, and replace logo SVG asset

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "../component/Navbar.jsx";
+import Hero from "../component/Hero.jsx";
 
 const Home = () => {
     return (
         <div>
             <Navbar/>
+            <Hero/>
         </div>
     );
 };
