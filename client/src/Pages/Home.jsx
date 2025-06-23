96b8cf3ec04852e0ca90d06c57cf6309dac9acfe
Karan Solanki <solan117@uwindsor.ca>
2025-06-23 18:04:53 -0400

    Add JobListing component to display and manage job search filters
    - Created `JobListing.jsx` to handle current search filters and job listing layout.
    - Integrated `JobListing` into `Home.jsx` component.

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from "../component/Navbar.jsx";
 import Hero from "../component/Hero.jsx";
+import JobListing from "../component/JobListing.jsx";
+
 
 const Home = () => {
     return (
         <div>
             <Navbar/>
             <Hero/>
+            <JobListing/>
         </div>
     );
 };
