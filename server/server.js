9f652deed924e9c5d3117f98cf38c954937a5c37
Karan Solanki <solan117@uwindsor.ca>
2025-06-29 16:34:00 -0400

    Set up basic Express server and update `package.json` scripts
    - Created `server.js` with Express server setup, CORS, and JSON middleware.
    - Configured a basic root route to verify API functionality.
    - Updated `package.json` to add a `server` script using Nodemon for development.

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+
+// Initialize express
+const app = express()
+
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+
+// Routes
+app.get('/', (req, res) => res.send("API WORKING"))
+
+
+// Port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
