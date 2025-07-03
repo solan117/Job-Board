c9458962864ea2907dd1b781072aeafa6b5682b2
Karan Solanki <solan117@uwindsor.ca>
2025-07-03 17:06:04 -0400

    Protect company routes with `authMiddleware`
    - Added `protectCompany` middleware to secure routes: `/company`, `/post-job`, `/applicants`, `/list-job`, `/change-status`, and `/change-visiblity`.

--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -10,6 +10,7 @@ import {
     registerCompany,
 } from '../controllers/companyController.js';
 import upload from '../config/multer.js';
+import {protectCompany} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
@@ -20,21 +21,21 @@ router.post('/register', upload.single('image'), registerCompany);
 router.post('/login', loginCompany);
 
 // Get company data
-router.get('/company', getCompanyData);
+router.get('/company', protectCompany, getCompanyData);
 
 // Post a job
-router.post('/post-job', postJob);
+router.post('/post-job', protectCompany, postJob);
 
 // Get Applicants Data of Company
-router.get('/applicants', getCompanyJobApplicants);
+router.get('/applicants', protectCompany, getCompanyJobApplicants);
 
 // Get Company Job List
-router.get('/list-job', getCompanyPostedJobs);
+router.get('/list-job', protectCompany, getCompanyPostedJobs);
 
 // Change Applications Status
-router.post('/change-status', ChangeJobApplicationStatus);
+router.post('/change-status', protectCompany, ChangeJobApplicationStatus);
 
 // Change Applications Visibility
-router.post('/change-visiblity', ChangeVisiblity);
+router.post('/change-visiblity', protectCompany, ChangeVisiblity);
 
 export default router;
