6fa3f3db67a95ad40f9f02ee98f5210e2d135e9a
Karan Solanki <solan117@uwindsor.ca>
2025-07-04 12:01:39 -0400

    Refine `JobApplication` model: update `userId`, `jobId`, and `status` definitions and add indexes for `jobId` and `companyId`.

--- server/models/JobApplication.js
+++ server/models/JobApplication.js
@@ -1,16 +1,31 @@
 import mongoose from "mongoose";
 
 const JobApplicationSchema = new mongoose.Schema({
-    userId: {type: String, ref: "User", required: true},
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
     companyId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Company",
         required: true,
     },
-    jobId: {type: mongoose.Schema.Types.ObjectId, ref: "Job", required: true},
-    status: {type: String, default: "Pending"},
+    jobId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job",
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["Pending", "Accepted", "Rejected"],
+        default: "Pending"
+    },
     date: {type: Number, required: true}
 });
+
+JobApplicationSchema.index({jobId: 1});
+JobApplicationSchema.index({companyId: 1});
 
 const JobApplication = mongoose.model('JobApplication', JobApplicationSchema)
 
