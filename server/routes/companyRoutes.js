bd42df5d6dc4c61adf6bf93a2f64c9cfdffedb2a
Karan Solanki <solan117@uwindsor.ca>
2025-07-04 11:01:51 -0400

    Update company route: rename `/company` endpoint to `/detail`

--- server/routes/companyRoutes.js
+++ server/routes/companyRoutes.js
@@ -21,7 +21,7 @@ router.post('/register', upload.single('image'), registerCompany);
 router.post('/login', loginCompany);
 
 // Get company data
-router.get('/company', protectCompany, getCompanyData);
+router.get('/detail', protectCompany, getCompanyData);
 
 // Post a job
 router.post('/post-job', protectCompany, postJob);
