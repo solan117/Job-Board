f634b5422c11eca2a4d70acff18233ad87f7a923
Karan Solanki <solan117@uwindsor.ca>
2025-07-08 12:06:59 -0400

    Restrict recruiter dashboard routes by redirecting unauthorized access to `/` using `Navigate`.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, {useContext} from "react";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import Application from "./Pages/Application.jsx";
 import ApplyJob from "./Pages/ApplyJob..jsx";
 import Home from "./Pages/Home.jsx";
@@ -27,15 +27,18 @@ const App = () => {
                 <Route path="/apply-job/:id" element={<ApplyJob/>}/>
 
                 <Route path='/dashboard' element={<Dashboard/>}>
-
-                    {
-                        companyToken ?
-                            <>
-                                <Route path='add-job' element={<AddJob/>}/>
-                                <Route path='manage-jobs' element={<ManageJobs/>}/>
-                                <Route path='view-applications' element={<ViewApplications/>}/>
-                            </> : null
-                    }
+                    <Route
+                        path='add-job'
+                        element={companyToken ? <AddJob/> : <Navigate to="/" replace/>}
+                    />
+                    <Route
+                        path='manage-jobs'
+                        element={companyToken ? <ManageJobs/> : <Navigate to="/" replace/>}
+                    />
+                    <Route
+                        path='job-list'
+                        element={companyToken ? <ViewApplications/> : <Navigate to="/" replace/>}
+                    />
                 </Route>
             </Routes>
         </div>
